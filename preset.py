f5272020fccad9b06cded2cd00223a7bae8e2a24
d-s-t <davidst360@gmail.com>
2025-01-06 12:21:49 +0200

    Update K particle mass to use energy in MeV units

--- preset.py
+++ preset.py
@@ -4,4 +4,4 @@ from constants import const, units
 H1 = Attom('H1', Z=1, A=1, R=0.88 * units.fm)
 H2 = Attom('H2', Z=1, A=2, R=2.14 * units.fm)
 He4 = Attom('He', Z=2, A=4, R=1.97 * units.fm)
-K = Particle('K', const.m_k, -1)
+K = Particle('K', 493.7 * units.MeVc2, -1)
