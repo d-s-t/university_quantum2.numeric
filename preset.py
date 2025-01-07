9a1b59bbbaf58313e6761a394937897b8763abe6
d-s-t <davidst360@gmail.com>
2025-01-07 13:22:43 +0200

    Update Particle instantiation in preset.py to use keyword arguments for mass and charge

--- preset.py
+++ preset.py
@@ -4,4 +4,4 @@ from utils import units
 H1 = Attom('H1', Z=1, A=1, R=0.88 * units.fm)
 H2 = Attom('H2', Z=1, A=2, R=2.14 * units.fm)
 He4 = Attom('He', Z=2, A=4, R=1.97 * units.fm)
-K = Particle('K', 493.7 * units.MeVc2, -1)
+K = Particle('K', m=493.7 * units.MeVc2, Z=-1)
