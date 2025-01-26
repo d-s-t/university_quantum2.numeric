01ac7685707cfb8fcc728425941ba6fcbd991a7a
d-s-t <davidst360@gmail.com>
2025-01-26 13:30:33 +0200

    Enhance Numerov class to support angular momentum dimensions in energy calculations and improve find_root method with DataFrame output

--- numerov_model.py
+++ numerov_model.py
@@ -5,6 +5,7 @@ from typing import Callable
 from classes import Particle
 from tqdm import tqdm
 from sys import float_info  
+from pandas import DataFrame
 
 class Numerov:
     """
@@ -35,6 +36,7 @@ class Numerov:
         self.V = V
         self.n = n
         self.l = l or np.arange(n, dtype=int)
+        self.l = self.l.reshape(1,1,-1)
 
     def W(self,
           E: np.ndarray[Quantity["MeV"]],
@@ -45,7 +47,7 @@ class Numerov:
 
         E: np.ndarray[Quantity["MeV"]]
             Energy of the particle
-            shape: (M,)
+            shape: (M,) or (M, L)
         r: np.ndarray[Quantity["fm"]]
             Array of distances
             shape: (N,)
@@ -55,8 +57,9 @@ class Numerov:
             shape: (N, M, L)
             where L is the number of angular momentum quantum numbers
         """
-        r = r[:, np.newaxis]
-        return (2 * self.particle.m * (E-self.V(r))[:, :, np.newaxis] / const.hbar**2 - (self.l * (self.l+1) / r**2)[:, np.newaxis, :]).to('fm-2')
+        r = r[:, np.newaxis, np.newaxis]
+        E = E.reshape(1, E.shape[0], -1)
+        return (2 * self.particle.m * (E-self.V(r)) / const.hbar**2 - (self.l * (self.l+1) / r**2)).to('fm-2')
 
 
     def ure(self,
@@ -72,53 +75,108 @@ class Numerov:
             shape: (N,)
         E: np.ndarray[float]
             Array of energies in MeV
-            shape: (M,)
+            shape: (M,) or (M, L)
         progress_bar: bool
             Whether to show a progress bar or not
         """
         r = r * self.particle.a_B
         dr = r[1] - r[0]
-        u = np.zeros((*r.shape, *E.shape, *self.l.shape)) * dr.unit
+        u = np.zeros((r.size, E.shape[0], self.l.size)) * dr.unit
         E = E * self.particle.R_y
-        u[1] = dr**(self.l+1)
+        u[1] = dr.unit * dr.value**(self.l[0]+1)
         w = dr**2 * self.W(E, r) / 12
         interations = tqdm(range(2, len(r))) if progress_bar else range(2, len(r))
         for i in interations:
             u[i, :] = ((2 - w[i-1,:] * 10) * u[i-1,:] - (1 + w[i-2,:]) * u[i-2,:]) / (1 + w[i,:])
-        return u / np.sqrt(np.trapezoid(u**2, r, axis=0))
+        return (u / np.sqrt(np.trapezoid(u**2, r, axis=0))).value
     
-    def find_root(self, E_max: float, E_min: float,
-                  r: np.ndarray[float],
-                  N: int = 10,
-                  ) -> np.ndarray[float]:
+    def find_root(self, E_max: float, E_min: float, r: np.ndarray[float], N: int = 10):
         """
         find the eneergy where the wave function u(R,E) = 0
 
-        E_max: float
+        E_max: float | np.ndarray[float]
             Maximum energy to search for the root as a multiple of the Rydberg energy
-        E_min: float
+            if array, shape: (L,) where L is the number of angular momentum quantum numbers
+        E_min: float | np.ndarray[float]
             Minimum energy to search for the root as a multiple of the Rydberg energy
+            if array, shape: (L,) where L is the number of angular momentum quantum numbers
         r: np.ndarray[float]
             Array of distances in fm
             shape: (M,)
         N: int (optional)
             Number of devisions to make in the search for the root
         """
+        E_list = []
+        self.find_root_helper(E_list, E_max*np.ones(self.l.size), E_min*np.ones(self.l.size), r, N)
+        df = DataFrame()
+        df['l'] = [l for l, _ in E_list]
+        df['E'] = [E for _, E in E_list]
+        return df
+        
+
+
+    def find_root_helper(self,
+                         E_list: list[tuple[int, np.ndarray[float]]],
+                         E_max: np.ndarray[float],
+                         E_min: np.ndarray[float],
+                         r: np.ndarray[float],
+                         N: int = 10,
+                         u_min: np.ndarray[float] = None,
+                         u_max: np.ndarray[float] = None
+                         ) -> None:
+        """
+        find the eneergy where the wave function u(R,E) = 0
+
+        E_max: float | np.ndarray[float]
+            Maximum energy to search for the root as a multiple of the Rydberg energy
+            if array, shape: (L,) where L is the number of angular momentum quantum numbers
+        E_min: float | np.ndarray[float]
+            Minimum energy to search for the root as a multiple of the Rydberg energy
+            if array, shape: (L,) where L is the number of angular momentum quantum numbers
+        r: np.ndarray[float]
+            Array of distances in fm
+            shape: (M,)
+        N: int (optional)
+            Number of devisions to make in the search for the root
+
+        u_min: np.ndarray[float] (optional)
+            wave function at the minimum energy
+            shape: (L,)
+
+        u_max: np.ndarray[float] (optional)
+            wave function at the maximum energy
+            shape: (L,)
+
+        returns: list[tuple[int, np.ndarray[float]]]
+            List of tuples with the angular momentum quantum number and the energy where the wave function is zero
+        """
+        mid = (E_max + E_min) / 2
+        for l in self.l.flatten():
+            if E_max[l] != np.nan and (mid[l] == E_max[l] or mid[l] == E_min[l]):
+                con = abs(u_min[l]) < abs(u_max[l])
+                E_list.append((l, E_min[l] * con + E_max[l] * ~con))
+                E_max[l] = E_min[l] = np.nan
+        if np.isnan(E_max).all():
+            print(flush=True)
+            return
+        print(f"diff={abs(E_max - E_min)}, {u_min=}, {u_max=}", end='\r', flush=True)
         E_bounds = np.linspace(E_min, E_max, N + 1)
-        u = self.ure(r, E_bounds, progress_bar=False)[-1,:]
-        u_min, u_max = u[0], u[-1]
-        print(f"diff={abs(E_max - E_min):.3e}, {u_min=:.3e}, {u_max=:.3e}", end='\r', flush=True)
-        if abs(E_max - E_min) <= float_info.epsilon:
-            print()
-            return [E_min] if abs(u_min) < abs(u_max) else [E_max]
-        return flatten([self.find_root(E_bounds[i+1], E_bounds[i], r, N) for i in range(N) if u[i]*u[i+1]<0])
+        # if len(E_bounds.shape) == 1:
+        #     E_bounds = E_bounds.reshape(-1, 1) * np.ones(self.l.size)
+        u = self.ure(r, E_bounds, progress_bar=False)[-1]
+        # u shape: (N, L)
+        # u_min, u_max shape: (L,)
+        for i in range(N):
+            if np.any(u[i]*u[i+1]<0):
+                self.find_root_helper(E_list, E_bounds[i], E_bounds[i+1], r, N, u[i], u[i+1])
         
     
-    def relative_error(self, E: float):
+    def relative_error(self, E: float, n: int=None) -> float:
         """
         relative error of the energy
         """
-        return abs(1 + self.n**2 * E)
+        n = n or self.n
+        return abs(1 + n**2 * E)
 
 
     @property
